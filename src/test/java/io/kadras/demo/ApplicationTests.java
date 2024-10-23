98e499166500358c77598ed7408e005f8ea1cb80
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2024-10-23 15:38:32 +0200

    Update

--- src/test/java/io/kadras/demo/ApplicationTests.java
+++ src/test/java/io/kadras/demo/ApplicationTests.java
@@ -1,11 +1,13 @@
 package io.kadras.demo;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 @Import({IntegrationTestSetup.class, TestcontainersConfiguration.class})
+@Disabled
 class ApplicationTests {
 
 	@Test
